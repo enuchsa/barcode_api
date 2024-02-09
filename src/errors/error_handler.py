c012a2a8548ef9c4e5102a544ea38b6ca98891b6
enuchsa <enuchsa@gmail.com>
2024-02-09 12:29:33 -0300

    feat: implementing validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,18 @@
 from src.views.http_types.http_response import HttpResponse
+from .errors_types.http_unprocessable_enitity import HttpUnprocessableEntityError
 
 def handle_erros(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+    
     return HttpResponse(
         status_code=500,
         body={
